f7c39e42d7457686dcfe171961cc15893e4cb774
sylpi10 <syl.pillet@hotmail.fr>
2020-07-29 22:41:54 +0200

    add cors

--- movierater/settings.py
+++ movierater/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -89,6 +91,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     }
 }
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:4200",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
